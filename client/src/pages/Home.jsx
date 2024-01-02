d1813d8eb48945aad37a1b6d151e86b99b56d7fc
Jerry <lpz.gerardo9@gmail.com>
2024-01-02 10:41:19 -0600

    client-home-page : Create empty member table
    Changes
    - Import table related componenets from mui
    - Create an empty table layout for members

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,39 @@
 import React from 'react';
+import Box from '@mui/material/Box';
+import TableContainer from '@mui/material/TableContainer';
+import Table from '@mui/material/Table';
+import TableHead from '@mui/material/TableHead';
+import TableBody from '@mui/material/TableBody';
+import TableRow from '@mui/material/TableRow';
+import TableCell from '@mui/material/TableCell';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
 
 const Home = () => {
     return(
         <React.Fragment>
-            <div>Hello from Home.</div>
+            <Box sx={{ width: '80vw', height: '800px' }}>
+                <Box>
+                    <TableContainer component={Paper}>
+                        <Table>
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>
+                                        <Typography>Member Table</Typography>
+                                    </TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                <TableRow>
+                                    <TableCell>
+                                        <Typography>Empty Table</Typography>
+                                    </TableCell>
+                                </TableRow>
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                </Box>
+            </Box>
         </React.Fragment>
     )
 }
