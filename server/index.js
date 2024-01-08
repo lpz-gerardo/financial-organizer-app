91e084d93288aab4cf5d51e8c8bf98e9538f073b
Jerry <lpz.gerardo9@gmail.com>
2024-01-08 11:45:21 -0600

    server-routes-setup : Create member route file
    Changes
    - Create member route file to handle member related routing
    - Add member route path to the index file

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import memberRoutes from '../server/routes/memberRoutes.js'
 const { MONGODB_URL, PORT } = process.env;
 
 const app = express();
@@ -12,6 +13,8 @@ app.use(cors());
 app.get('/', (request, response) => {
     response.send('Test connection.');
 })
+
+app.use('/member', memberRoutes);
 
 mongoose.connect(MONGODB_URL)
     .then(() => {
