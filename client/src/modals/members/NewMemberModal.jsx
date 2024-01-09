7208fc2c65f6a21061d9e442cca42622df33e22d
Jerry <lpz.gerardo9@gmail.com>
2024-01-09 10:17:29 -0600

    client-member-table : Create NewMemberModal jsx file

---
+++ client/src/modals/members/NewMemberModal.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NewMemberModal = () => {
+    return (
+        <React.Fragment>
+            <p>New Member Modal file</p>    
+        </React.Fragment>
+    )
+}
+
+export default NewMemberModal;
