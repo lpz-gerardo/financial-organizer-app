2177f573e9ff26f6ddf7ddab159139b5f5e659f4
Jerry <lpz.gerardo9@gmail.com>
2024-01-10 10:19:48 -0600

    server-member-routes : Create post member route
    Changes
    - Create async post member route function
    - Add try catch to handle any immediate errors
    - Add check to test if name already exists
    
    Notes
    - Future changes will refactor this logic to another file

--- server/routes/memberRoutes.js
+++ server/routes/memberRoutes.js
@@ -1,5 +1,34 @@
 import express from 'express';
+import { Member } from '../database/models/member.model.js';
 
 const router = express.Router();
+
+router.post('/', async (request, response) => {
+    try {
+        if (!request.body.name) {
+            return response.status(400).send({
+                message: 'Missing name'
+            });
+        }
+
+        if (await Member.exists({ name: request.body.name })) {
+            return response.status(400).send({
+                message: 'Name already exists.'
+            });
+        }
+
+        const newMember = {
+            name: request.body.name,
+            accounts: [],
+        };
+
+        const member = await Member.create(newMember);
+        return response.status(201).send(member);
+
+    } catch (error) {
+        console.log(error);
+        response.status(500).send({ message: error.message });
+    }
+})
 
 export default router;
