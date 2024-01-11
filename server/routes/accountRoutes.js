9af2c480eb0d06e837037949b7a3e17c18d367dc
Jerry <lpz.gerardo9@gmail.com>
2024-01-11 10:45:50 -0600

    server-account-routes : Create account post route
    Changes
    - Create a post route for adding new accounts to the DB

--- server/routes/accountRoutes.js
+++ server/routes/accountRoutes.js
@@ -1,5 +1,44 @@
 import express from 'express';
+import { Account } from '../database/models/account.model.js';
 
 const router = express.Router();
+
+router.post('/', async (request, response) => {
+    try {
+        if (!request.body.accountType
+            || !request.body.memberName
+            || !request.body.accountName
+            || !request.body.creditLimit
+            || !request.body.debt
+            || !request.body.monthlyPayment
+            || !request.body.annualPercentRate
+            || !request.body.paymentDay)
+            {
+                return response.status(400).send({
+                    message: 'Missing data.'
+                });
+            }
+
+            const newAccount = {
+                name: request.body.accountName,
+                accountType: request.body.accountType,
+                creditLimit: request.body.creditLimit,
+                startingDebt: request.body.debt,
+                remainingDebt: request.body.debt,
+                minimumMonthlyPayment: request.body.monthlyPayment,
+                annualPercentRate: request.body.annualPercentRate,
+                paymentDay: request.body.paymentDay,
+                memberName: request.body.memberName,
+            };
+
+            const account = await Account.create(newAccount);
+
+            return response.status(201).send(account);
+
+    } catch (error) {
+        console.log(error);
+        return response.status(500).send({ message: error.message });
+    }
+})
 
 export default router;
