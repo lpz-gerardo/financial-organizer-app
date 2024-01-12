fd7beb01b1b320423c1655ce45c9c435af4216cc
Jerry <lpz.gerardo9@gmail.com>
2024-01-12 09:31:10 -0600

    client-account-table : Create new account modal file

---
+++ client/src/modals/accounts/NewAccountModal.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NewAccountModal = () => {
+    return (
+        <React.Fragment>
+            <p>Account Modal</p>
+        </React.Fragment>
+    )
+}
+
+export default NewAccountModal;
