c0ee47829c4559d5654010438ff634908b2fd806
Jerry <lpz.gerardo9@gmail.com>
2024-01-18 10:23:58 -0600

    server-account-routes : Create accountController
    Changes
    - Create controllers directory in server directory
    - Create accountController file
    - Import account model DB functions
    - Create functions to handle checking necessary data is present before
      making any DB operations
    - Export the addAccount and getAccounts function to use in the routes
      file

---
+++ server/controllers/accountController.js
@@ -0,0 +1,54 @@
+import { createAccount, findAccounts } from '../database/models/account.model.js';
+
+const addAccount = async (request, response) => {
+    try {
+        if (!request.body.accountType
+            || !request.body.memberName
+            || !request.body.accountName
+            || !request.body.creditLimit
+            || !request.body.debt
+            || !request.body.monthlyPayment
+            || !request.body.annualPercentRate
+            || !request.body.paymentDay)
+            {
+                return response.status(400).send({
+                    message: 'Missing data.'
+                });
+            }
+
+            const newAccount = {
+                name: request.body.accountName,
+                accountType: request.body.accountType,
+                creditLimit: request.body.creditLimit,
+                startingDebt: request.body.debt,
+                remainingDebt: request.body.debt,
+                minimumMonthlyPayment: request.body.monthlyPayment,
+                annualPercentRate: request.body.annualPercentRate,
+                paymentDay: request.body.paymentDay,
+                memberName: request.body.memberName,
+            };
+
+            const account = await createAccount(newAccount);
+
+            return response.status(201).send(account);
+
+    } catch (error) {
+        console.log(error);
+        return response.status(500).send({ message: error.message });
+    }
+}
+
+const getAccounts = async (request, response) => {
+    try {
+        const accounts = await findAccounts();
+        return response.status(200).json({
+            count: accounts.length,
+            data: accounts,
+        });
+    } catch (error) {
+        console.log(error);
+        return response.status(500).send({ message: error.message });
+    }
+}
+
+export { addAccount, getAccounts };
