6d46a54924a6d3f8a99703fd36f77caffa2b3322
Jerry <lpz.gerardo9@gmail.com>
2024-01-19 11:55:24 -0600

    client-account-table : Add basic modal
    Changes
    - Add the basic modal structure
    - Add the modal style used for other modals

--- client/src/modals/accounts/DeleteAccountModal.jsx
+++ client/src/modals/accounts/DeleteAccountModal.jsx
@@ -1,8 +1,36 @@
 import React from 'react';
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+
+const modalStyle = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 350,
+    bgcolor: '#edf0f5',
+    boxShadow: 24,
+    padding: 8,
+}
 
 const DeleteAccountModal = () => {
     return (
         <React.Fragment>
+            <Modal>
+                <Box sx={modalStyle}>
+                    <Typography>Delete?</Typography>
+                    <Box>
+                        <Box>
+                            <Button>Cancel</Button>
+                        </Box>
+                        <Box>
+                            <Button>Delete</Button>
+                        </Box>
+                    </Box>
+                </Box>
+            </Modal>
         </React.Fragment>
     )
 }
