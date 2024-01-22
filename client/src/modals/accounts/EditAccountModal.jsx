8941781de38c6cb23f8559d4e78e302f9f782620
Jerry <lpz.gerardo9@gmail.com>
2024-01-22 10:47:40 -0600

    client-account-table : Create EditAccountModal

---
+++ client/src/modals/accounts/EditAccountModal.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const EditAccountModal = () => {
+    return (
+        <React.Fragment>
+            <p>Edit Account Modal</p>
+        </React.Fragment>
+    )
+}
+
+export default EditAccountModal;
