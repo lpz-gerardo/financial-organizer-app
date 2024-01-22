c4a7ea9c031c4d62608088dcbf42aff8b3392f80
Jerry <lpz.gerardo9@gmail.com>
2024-01-22 10:55:30 -0600

    client-account-table : Create layout of Edit Account Modal
    Changes
    - Import needed components
    - Add modal style
    - Create layout

--- client/src/modals/accounts/EditAccountModal.jsx
+++ client/src/modals/accounts/EditAccountModal.jsx
@@ -1,9 +1,41 @@
 import React from 'react';
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+
+const modalStyle = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 350,
+    bgcolor: '#edf0f5',
+    boxShadow: 24,
+    padding: 8,
+}
 
 const EditAccountModal = () => {
     return (
         <React.Fragment>
-            <p>Edit Account Modal</p>
+            <Modal>
+                <Box sx={modalStyle}>
+                    <Typography>Edit Account</Typography>
+                    <Box>
+                        <TextField></TextField>
+                    </Box>
+                    <Box>
+                        <TextField></TextField>
+                    </Box>
+                    <Box>
+                        <TextField></TextField>
+                    </Box>
+                    <Box>
+                        <Button>Submit</Button>
+                    </Box>
+                </Box>
+            </Modal>
         </React.Fragment>
     )
 }
