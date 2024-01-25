3ac9b6da9c73a2160101b071e0134f783ef47f86
Jerry <lpz.gerardo9@gmail.com>
2024-01-25 11:27:38 -0600

    server-member-routes : Remove business logic from route
    Changes
    - Remove business logic from the route path and replace with a call to
      the memberController that now contains the business logic
    - Remove member model import

--- server/routes/memberRoutes.js
+++ server/routes/memberRoutes.js
@@ -1,47 +1,12 @@
 import express from 'express';
-import { Member } from '../database/models/member.model.js';
-
+import {
+    addMember,
+    getMembers,
+} from '../controllers/memberController.js';
 const router = express.Router();
 
-router.post('/', async (request, response) => {
-    try {
-        if (!request.body.name) {
-            return response.status(400).send({
-                message: 'Missing name'
-            });
-        }
+router.post('/', addMember);
 
-        if (await Member.exists({ name: request.body.name })) {
-            return response.status(400).send({
-                message: 'Name already exists.'
-            });
-        }
-
-        const newMember = {
-            name: request.body.name,
-            accounts: [],
-        };
-
-        const member = await Member.create(newMember);
-        return response.status(201).send(member);
-
-    } catch (error) {
-        console.log(error);
-        return response.status(500).send({ message: error.message });
-    }
-})
-
-router.get('/', async (request, response) => {
-    try {
-        const members = await Member.find({});
-        return response.status(200).json({
-            count: members.length,
-            data: members,
-        });
-    } catch (error) {
-        console.log(error);
-        return response.status(500).send({ message: error.message });
-    }
-})
+router.get('/', getMembers);
 
 export default router;
