283d0b99c7d9be12c15b7c4c40a4fcef8fc674d7
Jerry <lpz.gerardo9@gmail.com>
2024-01-26 09:54:51 -0600

    client-member-table : Add base structure of file
    Changes
    - Add only the basic component and export it

---
+++ client/src/modals/members/EditMemberModal.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const EditMemberModal = () => {
+    return (
+        <React.Fragment>
+            <p>Edit Member Modal</p>
+        </React.Fragment>
+    )
+}
+
+export default EditMemberModal;
