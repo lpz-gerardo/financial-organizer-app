63880909dc6a452b4bac6e1d0abf924608c66cb7
Jerry <lpz.gerardo9@gmail.com>
2024-01-26 10:02:11 -0600

    client-member-table : Add layout of form
    Changes
    - Add the basic layout of the form in the modal
    - Import necessary MUI components for displaying form

--- client/src/modals/members/EditMemberModal.jsx
+++ client/src/modals/members/EditMemberModal.jsx
@@ -1,9 +1,44 @@
 import React from 'react';
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+
+const modalStyle = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 350,
+    bgcolor: '#edf0f5',
+    boxShadow: 24,
+    padding: 8,
+}
 
 const EditMemberModal = () => {
     return (
         <React.Fragment>
-            <p>Edit Member Modal</p>
+            <Modal>
+                <Box sx={modalStyle}>
+                    <Typography>
+                        Edit Member Name
+                    </Typography>
+                    <form>
+                        <Box>
+                            <TextField
+                                type='input'
+                                variant='outlined'
+                                color='primary'
+                                label='Name'
+                            />
+                        </Box>
+                        <Button>
+                            Submit
+                        </Button>
+                    </form>
+                </Box>
+            </Modal>
         </React.Fragment>
     )
 }
