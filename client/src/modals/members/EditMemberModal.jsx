1bdc4ac93921782691f8acbd8d1a268087e0ca45
Jerry <lpz.gerardo9@gmail.com>
2024-01-26 11:08:45 -0600

    client-member-table : Add form onSubmit function
    Changes
    - Add onSubmit function to form
      - Create handleEditSubmit function to prepare user input and call the
        updateMemberName function
      - Create updateMemberName function to make the PUT API call and
        refresh the tables

--- client/src/modals/members/EditMemberModal.jsx
+++ client/src/modals/members/EditMemberModal.jsx
@@ -6,6 +6,8 @@ import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 
+import { REACT_APP_DEV_URL } from '../../../config.js';
+
 const modalStyle = {
     position: 'absolute',
     top: '50%',
@@ -36,6 +38,30 @@ const EditMemberModal = ({ isModalOpen, handleClose, member, refreshData }) => {
         handleClose();
     }
 
+    const handleEditSubmit = (event) => {
+        event.preventDefault();
+        const data = {
+            'currentName': member,
+            'newName': memberName,
+        }
+        if (newName != '' && newName != currentName) {
+            updateMemberName(REACT_APP_DEV_URL + `member/${member}`, data);
+        }
+        onCloseModal();
+    }
+
+    async function updateMemberName(url, data) {
+        const response = await fetch(url, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        const member = await response.json();
+        console.log(member);
+        refreshData();
+    }
+
     return (
         <React.Fragment>
             <Modal open={isModalOpen} onClose={onCloseModal}>
@@ -43,7 +69,7 @@ const EditMemberModal = ({ isModalOpen, handleClose, member, refreshData }) => {
                     <Typography variant='h6' component='h2' color='black'>
                         Edit Member Name
                     </Typography>
-                    <form>
+                    <form onSubmit={handleEditSubmit}>
                         <Box>
                             <TextField
                                 type='input'
@@ -56,7 +82,7 @@ const EditMemberModal = ({ isModalOpen, handleClose, member, refreshData }) => {
                                 helperText={isMemberNameError ? 'Max 20 characters. Alpha only' : ''}
                             />
                         </Box>
-                        <Button disabled={isButtonDisabled}>
+                        <Button type='submit' disabled={isButtonDisabled}>
                             Submit
                         </Button>
                     </form>
