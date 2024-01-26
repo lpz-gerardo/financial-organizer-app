0690302c96afcbd894662f23d7c401b6f4ef863c
Jerry <lpz.gerardo9@gmail.com>
2024-01-26 11:55:35 -0600

    server-member-routes : Create editMember route
    Changes
    - Create a editMember function in memberController that handles the
      business logic of checking if the new name and current name are valid
    - Create a new route path that will redirect to the memberController

--- server/controllers/memberController.js
+++ server/controllers/memberController.js
@@ -1,7 +1,9 @@
 import {
     createMember,
     memberExists,
+    findMember,
     findMembers,
+    updateMember,
 } from '../database/models/member.model.js';
 
 const addMember = async (request, response) => {
@@ -39,4 +41,31 @@ const getMembers = async (request, response) => {
     }
 }
 
-export { addMember, getMembers, };
+const editMember = async (request, response) => {
+    try {
+        if (!request.params.currentName || !request.body.newName) {
+            return response.status(400).send({ message: 'Missing a name.' });
+        }
+
+        const filter = { name: request.params.currentName };
+        const member = await findMember(filter);
+        if (!member) {
+            return response.status(400).send({ message: 'Member not found.' });
+        }
+
+        const isNewNameTaken = await memberExists({ name: request.body.newName });
+        if (!isNewNameTaken) {
+            member.name = request.body.newName;
+            await updateMember(member);
+        } else {
+            return response.status(403).send({ message: 'Member name already exists.' });
+        }
+
+        return response.status(200).send({ message: 'Member name has been updated.' });
+    } catch (error) {
+        console.log(error);
+        return response.status(500).send({ message: error.message });
+    }
+}
+
+export { addMember, getMembers, editMember, };
--- server/routes/memberRoutes.js
+++ server/routes/memberRoutes.js
@@ -2,11 +2,14 @@ import express from 'express';
 import {
     addMember,
     getMembers,
+    editMember,
 } from '../controllers/memberController.js';
 const router = express.Router();
 
 router.post('/', addMember);
 
 router.get('/', getMembers);
+
+router.put('/:currentName', editMember);
 
 export default router;
