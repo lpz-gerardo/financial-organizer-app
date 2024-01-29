5524960f77c5eb3bf96047e4c48cb4fa27e593be
Jerry <lpz.gerardo9@gmail.com>
2024-01-29 10:55:09 -0600

    client-member-table : Add handleDelete function
    Changes
    - Add a handleDelete function to be called when the 'Delete' button is
      clicked
      - This function will get the selected member's name and fetch the
        DELETE API for removing members
    - Add onClick prop to the 'Cancel' button to close the modal safely

--- client/src/modals/members/DeleteMemberModal.jsx
+++ client/src/modals/members/DeleteMemberModal.jsx
@@ -4,6 +4,8 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 
+import { REACT_APP_DEV_URL } from '../../../config.js';
+
 const modalStyle = {
     position: 'absolute',
     top: '50%',
@@ -16,6 +18,29 @@ const modalStyle = {
 }
 
 const DeleteMemberModal = ({ isModalOpen, handleClose, member, refreshData }) => {
+
+    const handleDelete = () => {
+        const data = {
+            name: member,
+        }
+        deleteMember(REACT_APP_DEV_URL + `member/${member}`, data);
+        handleClose();
+    }
+
+    async function deleteMember(url, data) {
+        await fetch(url, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        }).then((response) => {
+            return response.json();
+        }).then(() => {
+            refreshData();
+        });
+    }
+
     return (
         <React.Fragment>
             <Modal open={isModalOpen} onClose={handleClose}>
@@ -26,10 +51,10 @@ const DeleteMemberModal = ({ isModalOpen, handleClose, member, refreshData }) =>
                     </Box>
                     <Box display='grid' gridTemplateColumns={'repeat(2, 1fr)'} gridTemplateRows={'1fr'} columnGap={2} rowGap={2} padding={2}>
                         <Box sx={{ gridColumn: 1 }}>
-                            <Button>Cancel</Button>
+                            <Button onClick={handleClose}>Cancel</Button>
                         </Box>
                         <Box sx={{ gridColumn: 2 }}>
-                            <Button variant='contained' color='error' >Delete</Button>
+                            <Button variant='contained' color='error' onClick={() => handleDelete()}>Delete</Button>
                         </Box>
                     </Box>
                 </Box>
