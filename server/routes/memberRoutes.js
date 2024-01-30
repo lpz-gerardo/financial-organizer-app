0389aa18a7cbc89048dc2c2a209bf6ffe836c1f8
Jerry <lpz.gerardo9@gmail.com>
2024-01-30 09:31:46 -0600

    server-member-routes : Add router delete path
    Changes
    - Add the router delete path for directing to the removeMember function
      in the member controller
      - Uses the member's name as the URL param as this is intended to be a
        unique value

--- server/routes/memberRoutes.js
+++ server/routes/memberRoutes.js
@@ -3,6 +3,7 @@ import {
     addMember,
     getMembers,
     editMember,
+    removeMember,
 } from '../controllers/memberController.js';
 const router = express.Router();
 
@@ -11,5 +12,7 @@ router.post('/', addMember);
 router.get('/', getMembers);
 
 router.put('/:currentName', editMember);
+
+router.delete('/:name', removeMember);
 
 export default router;
