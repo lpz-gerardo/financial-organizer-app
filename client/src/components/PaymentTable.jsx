4e365c04962030704699dc46bed02b5024f8ea24
Jerry <lpz.gerardo9@gmail.com>
2024-02-01 10:08:50 -0600

    client-payments : Add table layout
    Changes
    - Add the needed table imports from MUI
    - Create the layout of the table

--- client/src/components/PaymentTable.jsx
+++ client/src/components/PaymentTable.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import TableContainer from '@mui/material/TableContainer';
+import Table from '@mui/material/Table';
+import TableHead from '@mui/material/TableHead';
+import TableBody from '@mui/material/TableBody';
+import TableRow from '@mui/material/TableRow';
+import TableCell from '@mui/material/TableCell';
+import Typography from '@mui/material/Typography';
 
 const PaymentTable = () => {
     return (
         <React.Fragment>
-
+            <Box sx={{ maxWidth: '100%', maxHeight: '900px' }}>
+                <TableContainer component={Paper}>
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>
+                                    <Typography>Payments</Typography>
+                                </TableCell>
+                            </TableRow>
+                        </TableHead>
+                    </Table>
+                </TableContainer>
+            </Box>
         </React.Fragment>
     )
 }
