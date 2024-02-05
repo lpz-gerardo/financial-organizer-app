a13d79f5178c44aacfc6e6f110b9a4d20f59a197
Jerry <lpz.gerardo9@gmail.com>
2024-02-05 10:44:24 -0600

    client-util : Add formatPercent function
    Changes
    - Add a formatPercent function to format percentage to have
      - Up to two decimals
      - Append percent symbol at the end
      - Return as a string

--- client/src/util/formatter.js
+++ client/src/util/formatter.js
@@ -2,6 +2,11 @@ const formatMoney = (amount) => {
     return '$' + String(Number.parseFloat(amount).toFixed(2));
 }
 
+const formatPercent = (percent) => {
+    return String(Number.parseFloat(percent).toFixed(2) + '%');
+}
+
 export {
     formatMoney,
+    formatPercent,
 }
