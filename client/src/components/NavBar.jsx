e2c4be450f559c7b2dfef0e4b2b03f8145dc2973
Jerry <lpz.gerardo9@gmail.com>
2024-02-15 15:19:11 -0600

    client-home : Import AppBar
    Changes
    - Import AppBar from MUI library

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import AppBar from '@mui/material/AppBar';
 
 const NavBar = () => {
     return (
         <React.Fragment>
-
+            <AppBar component={'nav'}></AppBar>
         </React.Fragment>
     )
 }
