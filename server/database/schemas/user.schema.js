809de997dbd78418e03e95dcbd1774c0bc335d7f
Jerry <lpz.gerardo9@gmail.com>
2024-02-29 10:24:03 -0600

    server-auth : Add userSchema
    Changes
    - Add the userSchema using mongoose schema
    - Add the basic requirements
    - export the userSchema

---
+++ server/database/schemas/user.schema.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: new Date(),
+    },
+});
+
+export default userSchema;
