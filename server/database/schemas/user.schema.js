99d4ed5d9a89c61de6afe16de410444002adc62a
Jerry <lpz.gerardo9@gmail.com>
2024-02-29 11:50:14 -0600

    server-auth : hash passwords
    Changes
    - Add bcrypt hashing for user passwords before storing the passwords in
      the database
    - Require the username to be unique

--- server/database/schemas/user.schema.js
+++ server/database/schemas/user.schema.js
@@ -1,9 +1,11 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
 const userSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true,
+        unique: true,
     },
     password: {
         type: String,
@@ -13,6 +15,10 @@ const userSchema = new mongoose.Schema({
         type: Date,
         default: new Date(),
     },
+});
+
+userSchema.pre('save', async function () {
+    this.password = await bcrypt.hash(this.password, 12);
 });
 
 export default userSchema;
