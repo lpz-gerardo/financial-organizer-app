77dc3224731b5052334b78684f93dc07ddb720d4
Jerry <lpz.gerardo9@gmail.com>
2024-02-29 12:56:31 -0600

    server-auth : Add createSecretToken
    Changes
    - Add a createSecretToken function that will sign the jsonwebtoken

---
+++ server/utils/SecretToken.js
@@ -0,0 +1,10 @@
+import 'dotenv/config';
+import jwt from 'jsonwebtoken';
+
+const createSecretToken = (id) => {
+    return jwt.sign({ id }, process.env.TOKEN_KEY, {
+        expiresIn: process.env.EXPIRES_IN,
+    });
+};
+
+export default createSecretToken;
