8ef272c2e88f8e33c9e019de0cb40e6750c2adf4
Jerry <lpz.gerardo9@gmail.com>
2024-02-29 13:21:54 -0600

    server-auth : Add ability for user signup
    Changes
    - Add the ability for the user to be able to signup
    - Add a check to make sure the username being used does not already
      exist
    - Generate a secret token for the new user if sign up is successful
    - generate the cookie for the session
    - return a successful status code

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -4,9 +4,30 @@ import { createSecretToken } from '../utils/SecretToken.js';
 
 const Signup = async (request, response, next) => {
    try {
-        if (!request.body.username || !request.body.password) {
+        const { username, password, createdAt } = request.body;
+        if (!username || !password) {
             return response.status(400).send({ message: 'Missing username or password' });
         }
+
+        const doesUserExist = await User.findOne({ username });
+        if (doesUserExist) {
+            return response.status(400).send({ message: 'username already exists.' });
+        }
+
+        const user = await User.create({ username, password, createdAt });
+        const token = createSecretToken(user._id);
+
+        response.cookie('token', token, {
+            withCredentials: true,
+            httpOnly: false,
+        });
+
+        response.status(201).send({
+            message: 'User signed in successfully.',
+            success: true,
+            user
+        });
+        next();
    } catch (error) {
         return response.status(500).send({ message: error });
    }
