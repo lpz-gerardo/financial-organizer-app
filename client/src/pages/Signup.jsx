310e7a437dc1b9222a91748f8b4ee2e3bdebfccc
Jerry <lpz.gerardo9@gmail.com>
2024-03-04 14:27:20 -0600

    client-auth : Add form onSubmit
    Changes
    - Add a handleSubmit function that will call the signup API
    - Add onSubmit prop to the form and change the submit button type

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -5,6 +5,7 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import { REACT_APP_DEV_URL } from '../../config.js';
 
 const Signup = () => {
     const [userInputs, setUserInputs] = useState({
@@ -20,11 +21,19 @@ const Signup = () => {
         });
     }
 
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const data = {
+            'username': username,
+            'password': password,
+        };
+    }
+
     return(
         <Container sx={{ display: 'grid', justifyContent: 'center', }}>
             <Box sx={{ backgroundColor: 'white', padding: '50px', top: '50%', left: '50%', position: 'absolute' }}>
                 <Typography variant='h4' color={'black'}>Signup</Typography>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <Box marginTop={'15px'}>
                        <TextField
                             required
@@ -51,7 +60,7 @@ const Signup = () => {
                        />
                     </Box>
                     <Box marginTop={'10px'}>
-                        <Button>Submit</Button>
+                        <Button type='submit'>Submit</Button>
                     </Box>
                 </form>
                 <Box marginTop={'15px'}>
