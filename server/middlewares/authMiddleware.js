adfcd3277f51ebb239f9fa23a7c8c65d64a23c1c
Jerry <lpz.gerardo9@gmail.com>
2024-03-04 10:09:13 -0600

    server-auth : Add userVerification function
    Changes
    - Add an empty userVerification function
    - Import dotenv, jsonwebtoken, user model
    - export the userVerification function

---
+++ server/middlewares/authMiddleware.js
@@ -0,0 +1,11 @@
+import 'dotenv/config';
+import jwt from 'jsonwebtoken';
+import { User } from '../database/models/user.model.js';
+
+const userVerification = (request, response) => {
+
+}
+
+export {
+    userVerification,
+}
