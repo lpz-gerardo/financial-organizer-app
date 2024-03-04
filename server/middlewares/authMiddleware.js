5eb208da16091c21f7e4c384db6753fa6c20036b
Jerry <lpz.gerardo9@gmail.com>
2024-03-04 10:17:04 -0600

    server-auth : Add jsonwebtoken verification
    Changes
    - Add token verficiation logic
      - Check that a token exists
      - verify the token
      - return true only if the token is verified

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -3,7 +3,22 @@ import jwt from 'jsonwebtoken';
 import { User } from '../database/models/user.model.js';
 
 const userVerification = (request, response) => {
-
+    const token = request.cookies.token;
+    if (!token) {
+        return response.send({ status: false });
+    }
+    jwt.verify(token, process.env.TOKEN_KEY, async (error, data) => {
+        if (error) {
+            return response.send({ status: false });
+        } else {
+            const user = await User.findById(data.id)
+            if (user) {
+                return response.send({ status: true, user: user.username });
+            } else {
+                return response.send({ status: false });
+            }
+        }
+    });
 }
 
 export {
