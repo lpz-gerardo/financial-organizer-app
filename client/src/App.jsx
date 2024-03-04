b3d72243dabba2252ed314fc92766bdf436211e5
Jerry <lpz.gerardo9@gmail.com>
2024-03-04 11:05:45 -0600

    client-auth : Create client router
    Changes
    - Replace the home page with client-side routes for directing the user
      to different pages
      - Wrap the Home page in a route tag
      - Wrap the Signup page in a route tag

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import Signup from './pages/Signup';
 import './App.css'
 
 function App() {
-  return(
+  return (
    <React.Fragment>
-      <Home />
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/signup' element={<Signup />}/>
+    </Routes>
    </React.Fragment> 
   )
 } 
---
+++ client/src/pages/Signup.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Signup = () => {
+    return(
+        <p>Signup page</p>
+    );
+}
+
+export default Signup;
