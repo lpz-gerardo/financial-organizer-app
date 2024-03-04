a636d312a9c02bc72ac546b0a58fb4fac976cba6
Jerry <lpz.gerardo9@gmail.com>
2024-03-04 10:20:31 -0600

    server-auth : Add userVerification path
    Changes
    - Add the userVerification path to the auth router

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import { Signup, Login } from '../controllers/authController.js';
+import { userVerification } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
+router.post('/', userVerification);
 router.post('/signup', Signup);
 router.post('/login', Login);
 
