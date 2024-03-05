60f3ff2a2bfb24d987f8af39decd6838a77916f0
Jerry <lpz.gerardo9@gmail.com>
2024-03-05 15:15:27 -0600

    client-auth : Add css classes
    Changes
    - Add css classes to match the signup page
    - Import all necessary react mui components for basic layout
    - Create the basic layout of the login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,8 +1,30 @@
 import React from 'react';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
 const Login = () => {
     return (
-        <h1>Login</h1>
+        <Container className={'container-login'}>
+            <Box className={'box-login'}>
+                <Typography color={'black'} variant='h4'>Login</Typography>
+                <Box className={'box-login-fields'}>
+                    <TextField
+                        label='username'
+                    />
+                </Box>
+                <Box className={'box-login-fields'}>
+                    <TextField
+                        label='password'
+                    />
+                </Box>
+                <Box className={'box-login-fields'}>
+                    <Button>Login</Button>
+                </Box>
+            </Box>
+        </Container>
     )
 }
 
