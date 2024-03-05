1baefc19ecd5da1f16f68cae665e45fa4be39211
Jerry <lpz.gerardo9@gmail.com>
2024-03-05 14:41:48 -0600

    client-auth : Add login page route
    Changes
    - Add the login page to the client router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Signup from './pages/Signup';
+import Login from './pages/Login';
 import './App.css'
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/signup' element={<Signup />}/>
+      <Route path='/login' element={<Login />}/>
     </Routes>
    </React.Fragment> 
   )
