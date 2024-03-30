4591f9e795fe59c059efc42cbe4dac6f78b950d3
Jerry <lpz.gerardo9@gmail.com>
2024-03-30 11:35:37 -0500

    client-auth-setup : Add redux store to app
    Changes
    - Wrap the main app routes with the redux store using the react-redux
      Provider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,6 +8,8 @@ import {
   Route,
   RouterProvider,
  } from 'react-router-dom';
+ import store from './store.js';
+ import { Provider } from 'react-redux';
  import Home from './pages/Home.jsx';
  import Login from './pages/Login.jsx';
  import Signup from './pages/Signup.jsx';
@@ -20,10 +22,12 @@ const router = createBrowserRouter(
       <Route path='/signup' element={<Signup />} />
     </Route>
   )
-)
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={ router } />
-  </React.StrictMode>
-  );
+  <Provider store={ store }>
+    <React.StrictMode>
+      <RouterProvider router={ router } />
+    </React.StrictMode>
+  </Provider>
+);
