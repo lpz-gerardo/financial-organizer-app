d7e06156e065602d929fb117d1ea4b30789fef1a
Jerry <lpz.gerardo9@gmail.com>
2024-03-30 11:41:14 -0500

    client-auth-setup : Add logout functionality
    Changes
    - Create a logout button that will appear only when a user is logged in
    - Convert the title button to take the user to the home page
      - Also change the name to Fin View
    - Add useDispatch and useSelector from react redux
      - useSelector is to grab the logged in user's info if it is there
      - useDispatch is used to dispatch actions related to removing the
        credentials associated with the logged in user
    - Add logout mutation from the user api slice to call the backend
      endpoint

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,16 +1,51 @@
-import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { removeCredentials } from '../slices/authSlice';
+import { useLogoutMutation } from '../slices/usersApiSlice';
+
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 
 const NavBar = () => {
+    const { userInfo } = useSelector((state) => state.auth);
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const [logout] = useLogoutMutation();
+
+    const goToHomPage = () => {
+        navigate('/');
+    };
+
+    const handleLogout = async () => {
+        try {
+            await logout().unwrap();
+            dispatch(removeCredentials());
+            navigate('/');
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
     return (
         <Box marginBottom={'25px'}>
             <AppBar>
                 <Box  display={'grid'} gridTemplateColumns={'repeat(8, 1fr)'} alignItems={'center'} sx={{ height: '40px'}}>
                     <Box gridColumn={1}>
-                        <Typography>Home</Typography>
+                        <Button onClick={ goToHomPage }>
+                            <Typography color={'white'}>Fin View</Typography>
+                        </Button>
                     </Box>
+                    {userInfo && (
+                        <Box gridColumn={8}>
+                            <Button onClick={ handleLogout }>
+                                <Typography color={'white'}>Logout</Typography>
+                            </Button>
+                        </Box>
+                    )}
                 </Box>
             </AppBar>
         </Box>
