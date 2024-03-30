d4630c47ff787c51097676dddbbf52026dbe37bf
Jerry <lpz.gerardo9@gmail.com>
2024-03-30 11:38:01 -0500

    client-auth-setup : Move nav bar entire app
    Changes
    - Move the NavBar to the App file so it can be used in all pages
    - Add ToastContainer for using toast messages throughout the app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+
+import NavBar from './components/NavBar';
 import './App.css'
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <>
+      <NavBar />
+      <ToastContainer />
       <Outlet />
     </>
   )
