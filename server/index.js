a840c476a98c11b72895331e80fef13adc5bb748
Jerry <lpz.gerardo9@gmail.com>
2024-04-01 08:54:55 -0500

    server-auth-setup : Add cors
    Changes
    - Import cors and add it to the app

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -13,6 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(cors());
 
 app.use('/', authRoutes);
 app.use('/member', memberRoutes);
