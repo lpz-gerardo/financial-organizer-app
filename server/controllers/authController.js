b9fe950e42163c4b19b05da2a5dc5b5074ca02f0
Jerry <lpz.gerardo9@gmail.com>
2024-04-01 08:51:33 -0500

    server-auth-setup : Create logout user function
    Changes
    - Create a logout user function to handle removing the cookie
      associatated with the logged user
    - Fix typo in user is argument
    - remove password form the response object

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -31,18 +31,27 @@ const loginUser = asyncHandler(async (request, response) => {
         }
         const user = await User.findOne({ username });
         if (user && (await bcrypt.compare(password, user.password))) {
-            createSecretToken(response, user_id);
+            createSecretToken(response, user._id);
             response.status(201).json({
                 _id: user._id,
                 username,
-                password,
             });
         } else {
             response.status(403).json({ message: "Invalid username or password" });
         }
 });
 
+const logoutUser = asyncHandler(async (requeset, response) => {
+    response.cookie('token', '', {
+        httpOnly: true,
+        expires: new Date(0)
+    });
+    response.status(200).json({ message: 'User logged out' });
+});
+
+
 export {
     registerUser,
     loginUser,
+    logoutUser,
 }
