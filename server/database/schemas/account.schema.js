72f0966fb942f64c3450acbc395e838f4e1b2b80
Jerry <lpz.gerardo9@gmail.com>
2024-04-08 10:36:33 -0500

    server-db : Add memberId and userId properties
    Changes
    - Add a memberId property that references the member that the account
      was created under
    - Add a userId property that references the user that the account was
      created under
    - Update all properties to contain more restrictions
      - minimum limits
      - required

--- server/database/schemas/account.schema.js
+++ server/database/schemas/account.schema.js
@@ -1,24 +1,68 @@
 import mongoose from 'mongoose';
 
 const accountSchema = new mongoose.Schema({
-    name: String,
+    name: {
+        type: String,
+        min: 3,
+        max: 50,
+        required: true,
+    },
     accountType: {
         type: String,
         enum: ["credit", "loan"],
+        required: true,
     },
-    creditLimit: Number,
-    startingDebt: Number,
-    remainingDebt: Number,
-    minimumMonthlyPayment: Number,
-    annualPercentRate: Number,
-    paymentDay: Number,
-    memberName: String,
-    remainingPayments: Number,
-    lengthOfLoan: Number,
-    memberId: {
+    creditLimit: {
+        type: Number,
+        min: 0,
+        required: true,
+    },
+    startingDebt: {
+        type: Number,
+        min: 0,
+        required: true,
+    },
+    remainingDebt: {
+        type: Number,
+        min: 0,
+        required: true,
+    },
+    minimumMonthlyPayment: {
+        type: Number,
+        min: 0,
+        required: true,
+    },
+    annualPercentRate: {
+        type: Number,
+        min: 0,
+        required: true,
+    },
+    paymentDay: {
+        type: Number,
+        min: 1,
+        max: 31,
+        required: true,
+    },
+    remainingPayments: {
+        type: Number,
+        min: 0
+    },
+    lengthOfLoan: {
+        type: Number,
+        min: 0,
+    },
+    memberName: {
         type: String,
         required: true,
-    }
+    },
+    memberId: {
+        type: mongoose.Types.ObjectId,
+        ref: 'Member',
+    },
+    userId: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User', 
+    },
 });
 
 export default accountSchema;
