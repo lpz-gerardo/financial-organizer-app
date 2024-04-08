42ad4e80a0fadf9030c22642000ab2aa7973027d
Jerry <lpz.gerardo9@gmail.com>
2024-04-08 10:46:55 -0500

    server-db : Rename params
    Changes
    - Rename the params to follow express approach

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -2,21 +2,21 @@ import jwt from 'jsonwebtoken';
 import asyncHandler from 'express-async-handler';
 import { User } from '../database/models/user.model.js';
 
-const userVerification = asyncHandler(async (request, response, next) => {
-    const token = request.cookies.token;
+const userVerification = asyncHandler(async (req, res, next) => {
+    const token = req.cookies.token;
     if (token) {
         try {
             const decoded = jwt.verify(token, process.env.TOKEN_KEY);
 
-            request.user = await User.findById(decoded.userId).select('-password');
+            req.user = await User.findById(decoded.userId).select('-password');
 
             next();
         } catch (error) {
-            response.status(401);
+            res.status(401);
             console.log('Not authorized, invalid token');
         }
     } else {
-        response.status(401);
+        res.status(401);
         console.log('Not authorized, no token');
     }
 });
