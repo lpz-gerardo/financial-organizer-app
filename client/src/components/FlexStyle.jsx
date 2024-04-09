268217ec8a809722e085675a6c17fb7afec29bae
Jerry <lpz.gerardo9@gmail.com>
2024-04-09 11:00:56 -0500

    client-navbar : Create reusable flexbox
    Changes
    - Create a reusable flex styled box component to help with managing the
      centering and distribution of components throughout the app

---
+++ client/src/components/FlexStyle.jsx
@@ -0,0 +1,10 @@
+import Box from '@mui/material/Box';
+import { styled } from '@mui/material';
+
+const FlexStyle = styled(Box)({
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+});
+
+export default FlexStyle;
