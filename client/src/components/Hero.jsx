17c5211233089cfb88f72d84fce5c4b41188963d
Jerry <lpz.gerardo9@gmail.com>
2024-04-09 13:27:52 -0500

    client-home : Update Hero
    Changes
    - Update the design and layout of the Hero page from the starter layout
    - Include the themeSettings and apply the color scheme

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,23 +1,48 @@
-import { Container, Button, Box } from '@mui/material';
+import { Button, Box, Typography } from '@mui/material';
+import { useTheme } from '@mui/material';
 
 const Hero = () => {
-    return (
-        <>
-        <h1>Fin View</h1>
-        <Container>
-            <Box>
-                <Button href='/login'>
-                    Login
-                </Button>
-            </Box>
-            <Box>
-                <Button href='/signup'>
-                    Sign up
-                </Button>
-            </Box>
-        </Container>
-        </>
-    );
+	const theme = useTheme();
+
+  return (
+    <>
+      <Box
+        sx={{
+          m: '1.5rem 2.5rem',
+          border: '2px solid',
+          bgcolor: theme.palette.neutral.dark,
+          width: '400px',
+          height: '400px',
+          borderRadius: '0.55rem',
+        }}
+      >
+        <Box m={'5rem'}>
+          <Typography
+            fontFamily={theme.typography.h1}
+            color={theme.palette.neutral.light}
+          >
+            FIN VIEW
+          </Typography>
+        </Box>
+      <Box display={'inline-flex'}>
+        <Box m={'1rem'}>
+          <Button href='/login' variant='outlined'>
+            <Typography>
+              Login
+            </Typography>
+          </Button>
+        </Box>
+        <Box m={'1rem'}>
+          <Button href='/signup' variant='outlined'>
+            <Typography>
+              Sign up
+            </Typography>
+          </Button>
+        </Box>
+      </Box>
+      </Box>
+    </>
+  );
 };
 
 export default Hero;
