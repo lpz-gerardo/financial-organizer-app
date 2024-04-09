40cc40f6de63e1212e482651519563ccdc3f68c9
Jerry <lpz.gerardo9@gmail.com>
2024-04-09 11:02:05 -0500

    client-navbar : Update NavBar
    Changes
    - Update the NavBar design and color scheme using the updated theme
    - Add A menu icon button to be used for opening a side bar
    - Add An Avatar button to be used as a drop down menu for user related
      links
      - Avatar uses the first letter of the username to display a character
      - Avatar only appears if a user is logged in
      - Drop down currently has the logout link
    - Make use of the FlexStyle component to distribute the navbar spacing
      - Remove previously used grid system
    - Add toast messages for errors

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,58 +1,92 @@
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
+import { toast } from 'react-toastify';
 import { removeCredentials } from '../slices/authSlice';
 import { useLogoutMutation } from '../slices/usersApiSlice';
+import FlexStyle from './FlexStyle';
 
 import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import Avatar from '@mui/material/Avatar';
+import Menu from '@mui/material/Menu';
+import LogoutOutlined from '@mui/icons-material/LogoutOutlined';
+import MenuItem from '@mui/material/MenuItem';
 import { useTheme } from '@mui/material';
+import { Menu as MenuIcon } from '@mui/icons-material';
 
 const NavBar = () => {
-    const { userInfo } = useSelector((state) => state.auth);
+	const [anchorEl, setAnchorEl] = useState(null);
+	const isOpen = Boolean(anchorEl);
 
-    const theme = useTheme();
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
+	const theme = useTheme();
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
 
-    const [logout] = useLogoutMutation();
+	const [logout] = useLogoutMutation();
+	const { userInfo } = useSelector((state) => state.auth);
 
-    const goToHomPage = () => {
-        navigate('/');
-    };
+	const handleOpenMenu = (e) => setAnchorEl(e.currentTarget);
+	const handleCloseMenu = () => setAnchorEl(null);
 
-    const handleLogout = async () => {
-        try {
-            await logout().unwrap();
-            dispatch(removeCredentials());
-            navigate('/');
-        } catch (err) {
-            console.log(err);
-        }
-    };
+	const handleNavigateHome = () => navigate('/');
 
-    return (
-        <Box marginBottom={'25px'}>
-            <AppBar>
-                <Box  display={'grid'} gridTemplateColumns={'repeat(8, 1fr)'} alignItems={'center'} sx={{ height: '40px'}}>
-                    <Box gridColumn={1}>
-                        <Button onClick={ goToHomPage }>
-                            <Typography color={theme.palette.neutral.light}>Fin View</Typography>
-                        </Button>
-                    </Box>
-                    {userInfo && (
-                        <Box gridColumn={8}>
-                            <Button onClick={ handleLogout }>
-                                <Typography color={theme.palette.neutral.light}>Logout</Typography>
-                            </Button>
-                        </Box>
-                    )}
-                </Box>
-            </AppBar>
-        </Box>
+	const handleLogout = async () => {
+		try {
+			await logout().unwrap();
+			dispatch(removeCredentials());
+			navigate('/');
+		} catch (err) {
+			toast.error(err.error);
+		}
+	};
 
-    )
+	return (
+		<AppBar sx={{ bgcolor: theme.palette.neutral.dark}}>
+			<Toolbar sx={{ justifyContent: 'space-between'}}>
+				<FlexStyle>
+					<IconButton>
+						<MenuIcon sx={{ color: theme.palette.neutral.light}}/>
+					</IconButton>
+				<FlexStyle>
+					<Button onClick={handleNavigateHome}>
+						<Typography color={theme.palette.primary} fontFamily={theme.typography.h2}>Fin View</Typography>
+					</Button>
+				</FlexStyle>
+				</FlexStyle>
+
+				{userInfo && (
+				<FlexStyle gap={"1.5rem"}>
+					<IconButton onClick={handleOpenMenu}>
+						<Avatar
+							sx={{
+								bgcolor: theme.palette.primary.main,
+								color: theme.palette.neutral.dark}}
+						>
+							<Typography fontFamily={theme.typography.h3}>
+							{userInfo.username[0].toUpperCase()}
+							</Typography>
+						</Avatar>
+					</IconButton>
+					<Menu
+						anchorEl={anchorEl}
+						open={isOpen}
+						onClose={handleCloseMenu}
+						anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+					>
+						<MenuItem onClick={handleLogout}>
+							<LogoutOutlined />
+							Logout
+						</MenuItem>
+					</Menu>
+				</FlexStyle>
+				)}
+			</Toolbar>
+		</AppBar>
+	)
 }
 
 export default NavBar;
