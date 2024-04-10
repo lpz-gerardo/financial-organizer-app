d861652009e1fbc15d4391db8b94259f4708aade
Jerry <lpz.gerardo9@gmail.com>
2024-04-10 09:06:17 -0500

    client-home : Update layout of Hero
    Changes
    - Update the display layout of the Hero page.
    - Make changes to the color theme
    - Change indentation to 2 spaces

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,46 +1,58 @@
 import { Button, Box, Typography } from '@mui/material';
 import { useTheme } from '@mui/material';
+import FlexStyle from './FlexStyle';
 
 const Hero = () => {
 	const theme = useTheme();
 
   return (
     <>
-      <Box
-        sx={{
-          m: '1.5rem 2.5rem',
-          border: '2px solid',
-          bgcolor: theme.palette.neutral.dark,
-          width: '400px',
-          height: '400px',
-          borderRadius: '0.55rem',
-        }}
-      >
-        <Box m={'5rem'}>
-          <Typography
-            fontFamily={theme.typography.h1}
-            color={theme.palette.neutral.light}
-          >
-            FIN VIEW
-          </Typography>
-        </Box>
-      <Box display={'inline-flex'}>
-        <Box m={'1rem'}>
-          <Button href='/login' variant='outlined'>
-            <Typography>
-              Login
+    <Box sx={{ display: 'block', width: '100%' }}>
+
+      <FlexStyle sx={{ 
+        justifyContent: 'center',
+        boxSizing: 'border-box',
+      }}>
+        <Box
+          sx={{
+            m: '1.5rem 2.5rem',
+            border: '1px solid',
+            borderColor: theme.palette.primary.dark,
+            bgcolor: theme.palette.grey[50],
+            width: '400px',
+            height: '400px',
+            borderRadius: '0.55rem',
+          }}
+        >
+          <Box m={'5rem'}>
+            <Typography
+              fontFamily={theme.typography.h1}
+              fontWeight={'bold'}
+              color={theme.palette.primary.dark}
+            >
+              FIN VIEW
             </Typography>
-          </Button>
+          </Box>
+          <Box display={'inline-flex'}>
+            <Box m={'1rem'}>
+              <Button href='/login' variant='contained' sx={{ bgcolor: theme.palette.primary.main }}>
+                <Typography>
+                  Login
+                </Typography>
+              </Button>
+            </Box>
+            <Box m={'1rem'}>
+              <Button href='/signup' variant='contained' sx={{ bgcolor: theme.palette.primary.main }}>
+                <Typography>
+                  Sign up
+                </Typography>
+              </Button>
+            </Box>
+          </Box>
         </Box>
-        <Box m={'1rem'}>
-          <Button href='/signup' variant='outlined'>
-            <Typography>
-              Sign up
-            </Typography>
-          </Button>
-        </Box>
-      </Box>
-      </Box>
+      </FlexStyle>
+    </Box>
+
     </>
   );
 };
