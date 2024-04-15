ed221960d63c70cf276364538899228e70595ec8
Jerry <lpz.gerardo9@gmail.com>
2024-04-15 09:28:08 -0500

    client-layout : Add domain to token
    Changes
    - Add a localhost domain value to the token cookie
    - Add a base path to the token cookie
    - Rename response paramater to res
      - this better follows documentation
    - Replace object export with default export

--- server/utils/SecretToken.js
+++ server/utils/SecretToken.js
@@ -1,18 +1,18 @@
 import jwt from 'jsonwebtoken';
 
-const createSecretToken = (response, userId) => {
+const createSecretToken = (res, userId) => {
     const token = jwt.sign({ userId }, process.env.TOKEN_KEY, {
         expiresIn: process.env.EXPIRES_IN
     });
 
-    response.cookie('token', token, {
+    res.cookie('token', token, {
         httpOnly: true,
         secure: false,
         sameSite: 'strict',
+        domain: 'localhost',
+        path: '/',
         maxAge: 10 * 24 * 60 * 60 * 1000
     });
 };
 
-export {
-    createSecretToken
-}
+export default createSecretToken;
