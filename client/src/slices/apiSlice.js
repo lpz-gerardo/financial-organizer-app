d8c2c1550c75f9e70d77509b696759868f781b8c
Jerry <lpz.gerardo9@gmail.com>
2024-04-30 13:39:57 -0500

    client-layout : Add base url to api slice
    Changes
    - Add the base url from the config file to the api slice
    - Add Member tag to api slice

--- client/src/slices/apiSlice.js
+++ client/src/slices/apiSlice.js
@@ -1,9 +1,10 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { REACT_APP_DEV_URL } from '../../config';
 
-const baseQuery = fetchBaseQuery({ baseUrl: '' });
+const baseQuery = fetchBaseQuery({ baseUrl: `${REACT_APP_DEV_URL}`, credentials: 'include' });
 
 export const apiSlice = createApi({
     baseQuery,
-    tagTypes: ['User'],
+    tagTypes: ['User', 'Member'],
     endpoints: (builder) => ({}),
 });
