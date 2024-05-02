56eb3668b073fb20c25138a87614a6130075f07c
Jerry <lpz.gerardo9@gmail.com>
2024-05-02 09:32:56 -0500

    server-member-grid : Update auth route url
    Changes
    - Add user to the auth routes url
    - Add origin and credentials to cors

--- server/index.js
+++ server/index.js
@@ -14,9 +14,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+}));
 
-app.use('/', authRoutes);
+app.use('/user', authRoutes);
 app.use('/member', memberRoutes);
 app.use('/account', accountRoutes);
 
