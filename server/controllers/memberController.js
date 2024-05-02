9fd0f03b0098a97e8384cc62200b1afb93c0a4a0
Jerry <lpz.gerardo9@gmail.com>
2024-05-02 10:12:13 -0500

    server-member-grid : Update function requests
    Changes
    - Update the data the functions get from the requests
      - With redux store the data being send to the API is slightly
        different
    - Update the status codes to be more accurate

--- server/controllers/memberController.js
+++ server/controllers/memberController.js
@@ -30,7 +30,7 @@ const addMember = async (req, res) => {
             accounts: [],
         }
 
-        const result = createMember(newMember);
+        const result = await createMember(newMember);
         res.status(201).json(result);
     } catch (error) {
         console.log(error);
@@ -40,39 +40,46 @@ const addMember = async (req, res) => {
 
 const getMembers = async (req, res) => {
     try {
-        const { userId } = req.body;
+        const userId = req.user._id;
         const members = await findMembers({ userId: userId });
         res.status(200).json({
             data: members,
         });
     } catch (error) {
         console.log(error);
-        return res.status(500).json({ message: error.message });
+        res.status(500).json({ message: error.message });
     }
 }
 
 const editMember = async (req, res) => {
     try {
-        if (!req.params.currentName || !req.body.newName) {
+        if (!req.body.newName) {
             return res.status(400).json({ message: 'Missing a name.' });
         }
 
-        const filter = { name: req.params.currentName };
+        if (!req.body.memberId) {
+            return res.status(400).json({ message: 'There was an error with the existing member.' });
+        }
+
+        const filter = { userId: req.body.userId, _id: req.body.memberId };
         const member = await findMember(filter);
         if (!member) {
             return res.status(400).json({ message: 'Member not found.' });
         }
 
-        const isNewNameTaken = await memberExists({ name: req.body.newName });
+        const isNewNameTaken = await memberExists({ name: req.body.newName, userId: req.body.userId });
         if (!isNewNameTaken) {
             member.name = req.body.newName;
-            await updateAccounts({ memberName: req.params.currentName }, { memberName: member.name });
+            await updateAccounts(filter, { memberName: member.name });
             await updateMember(member);
         } else {
-            return res.status(403).json({ message: 'Member name already exists.' });
+            return res.status(400).json({ message: 'Member name already exists.' });
         }
 
-        return res.status(200).json({ message: 'Member name has been updated.' });
+        return res.status(200).json({
+            member: member,
+            message: 'Member name has been updated.'
+        });
     } catch (error) {
         console.log(error);
         return res.status(500).json({ message: error.message });
@@ -81,13 +88,12 @@ const editMember = async (req, res) => {
 
 const removeMember = async (req, res) => {
     try {
-        const conditions = { name: req.params.name };
+        const conditions = { _id: req.body.memberId, userId: req.body.userId };
         const result = await deleteMember(conditions);
         if (!result) {
-            return res.status(404).json({ message: 'Member does not exist.' });
+            return res.status(400).json({ message: 'Member does not exist.' });
         }
 
-        await deleteAccounts({ memberName: req.params.name });
         return res.status(200).json({ message: 'Member was successfully deleted.' });
     } catch (error) {
         console.log(error);
